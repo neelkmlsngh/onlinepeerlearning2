a03ae4eb545307c26c13ef803583f0e3377c395e
traning <shivam17bajpai@bajpai.com>
2017-10-27 08:44:46 +0530

    separating editor component

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { config } from '../shared/config/config';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  languages:any=[];
   constructor() { }
 
   ngOnInit() {
+  	this.languages=config.language;
   }
 
 }
--- client/src/app/shared/components/editor/editor.component.ts
+++ client/src/app/shared/components/editor/editor.component.ts
@@ -3,11 +3,6 @@ import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core
 import { FormsModule } from '@angular/forms';
 import { config } from './../../config/config';
 
-/*import 'brace';
-import 'brace/ext/language_tools';
-import 'brace/mode/html';
-import 'ace-builds/src-min-noconflict/snippets/html';*/
-
 
 import { EditorService } from '../../services/editor.service';
 import { GitService } from '../../services/git.service'
@@ -22,7 +17,6 @@ import { GitService } from '../../services/git.service'
 export class EditorComponent implements OnInit {
   notebook:any;
   content:any="Please enter your code here";
-  languages:any=[];
 
   constructor(private editorService: EditorService, private gitService: GitService) {}
 
@@ -34,66 +28,17 @@ export class EditorComponent implements OnInit {
     })
   }
 
-  changeContent(){
+/*  changeContent(){
     this.content="Content changed";
     this.notebook.setSource(this.content);
   }
-  
-  //declaring variables
-
- //  enter:string;
- //  lang: any;
- //  url: any = "";
-
- //  text: string ="enter code here";
-  
-
- //  output:any;
- //  value:{};
- //    windowRef:any;
-  // screenSharingLink:any;
-
- //  options: any = {
- //    maxLines: 1000, 
- //    printMargin: false,
- //  };
-  
- //  val:any
-
-
-  //creating the instances of services
-
-
-
-  //method to execute the code 
-  // run(text) {
-  //   this.enter = "output"
-  //   this.value = {
-  //     run: text
-
-  //   }
-  //   this.editorService.runCode(this.value).subscribe(data => {
-  //     this.output = data.result;
-  //     console.log(this.output)
-  //   }, err => this.output = err)
-  // }
-
-  //method to clear the terminal
-  clear() {
-  //   console.log(this.text)
-  //   this.text = null;
-  //   console.log(this.text)
-  }
-
+ */
+ 
   //method to get github repositories
   ngOnInit() {
-    this.languages=config.language;
+    
     // this.lang = "javascript";
     
   }
 
-
-  mode() {
-    // this.lang = language;
-  }
 }
