95f38bd90ebf4435a40f74e05e335420406b6f36
traning <shivam17bajpai@bajpai.com>
2017-10-27 19:54:22 +0530

    remove style

--- client/src/app/shared/components/repo-sidebar/repo-sidebar.component.ts
+++ client/src/app/shared/components/repo-sidebar/repo-sidebar.component.ts
@@ -21,49 +21,46 @@ githubUser: any;
 @Output() content  = new EventEmitter<any>();
 
   constructor(private editorService: EditorService, private gitService: GitService) { }
-
 ngOnInit() {
-	this.gitService.getRepos()
-      .subscribe(repos => {
-        this.githubUser = repos;
+    this.gitService.getRepos()
+     .subscribe(repos => {
+       this.githubUser = repos;
+       console.log(repos)
 
-      })
-             this.content.emit(this.text);
-  }
-
+     })
+ }
 
 reposearch()
-			{
-				
-				console.log(this.selectedValue)
-				this.gitService.getTree(this.selectedValue)
-				.subscribe(data=>this.data=data)
-			}
+            {                
+                console.log(this.selectedValue)
+                this.gitService.getTree(this.selectedValue)
+                .subscribe(data=>this.data=data)
+                console.log(this.data)
+            }
 
+ showFile(reponame, filename) {
+   this.gitService.openFolder(reponame, filename)
+     .subscribe(
+       data => {
+         this.data = data
+         this.url = this.url + filename + "/"
+         console.log(this.url)
+       }
+       , err => {
+         this.show(reponame, this.url + filename)
+         this.url = "";
+       })
+ }
 
+ show(reponame, filename) {
+   this.gitService.getFile(reponame, filename)
+     .subscribe(data => {
 
-  showFile(reponame, filename) {
-    this.gitService.openFolder(reponame, filename)
-      .subscribe(
-        data => {
-          this.data = data
-          this.url = this.url + filename + "/"
-          console.log(this.url)
-        }
-        , err => {
-          this.show(reponame, this.url + filename)
-          this.url = "";
-        })
-  }
-
-
-  show(reponame, filename) {
-    this.gitService.getFile(reponame, filename)
-      .subscribe(data => {
-        this.fileData = data;
-        this.text = this.fileData._body;
-        this.content.emit(this.text);
-      })
-  }
+       this.fileData = data;
+       this.text = this.fileData._body;
+       this.content.emit(this.text);
+       console.log(JSON.stringify(this.text))
+     })
+ }
   
 }
