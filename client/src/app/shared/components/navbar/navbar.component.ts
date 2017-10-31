0cd798a0d18fcd470be68da8126d8ffb6130123d
Anjaliani <singhanjali00021@gmail.com>
2017-10-31 15:28:11 +0530

    swal added

--- client/src/app/shared/components/navbar/navbar.component.ts
+++ client/src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import swal from 'sweetalert2';
 import {AuthenticationService} from '../../../shared/services/authentication.service'
+import swal from 'sweetalert2';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -22,24 +22,12 @@ logout(){
     }
      
 		this.authenticationservice.logoutEditor(user).subscribe((data1)=>{
-      //We get dialog result
-        if (data1) {
-          swal({
-      timer: 1000,
-      title: "Preferences are set",
-      text:  "accepted",
-      type:  'success',
-      showConfirmButton: false,
-    })
-        } else {
-          swal({
-      timer: 1000,
-      title: "Preferences are not set",
-      text:  "declined",
-      type: 'error',
-      showConfirmButton: false,
-    })
-        }
+      swal({
+            timer: 2000,
+            title: "Successfully Logged Out",
+            type: 'success',
+            showConfirmButton: false,
+          })
     this.router.navigate(["/"]);
      localStorage.removeItem('currentUser');
 })
