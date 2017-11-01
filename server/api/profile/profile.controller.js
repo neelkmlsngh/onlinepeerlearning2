b9f7cd8c5465c4e383fa6156049e37fed8faa3a3
shivani <shivanisah96@gmail.com>
2017-11-01 10:19:12 +0530

    update profile component

--- server/api/profile/profile.controller.js
+++ server/api/profile/profile.controller.js
@@ -2,6 +2,8 @@ const ProfileModel = require('../users/users.entity');
 const logger = require('../../services/app.logger');
 const appConstant = require('../../config').app;
 const ProfileUser = require('./profile.entity')
+
+
 const getProfile = function(getId) {
     console.log(getId + '65gt65')
     return new Promise((resolve, reject) => {
