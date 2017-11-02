48295dd6bb6da210fdbd5f574422d9956ec7a8cc
pulkit.5.kakkar <pulkit.5.kakkar@niit-tech.com>
2017-11-02 15:28:10 +0530

    indentation done for the chat server files

--- server/api/chat/chat.controller.js
+++ server/api/chat/chat.controller.js
@@ -4,77 +4,73 @@ const loginModel = require('../login/login.entity')
 const chatMsgModel = require('./chat.entity')
 
 const userSessionCheck = function(data, callback) {
-    loginModel.findOne({ "userId": data.userId }, (err, result) => {
-        callback(err, result);
-    })
+	loginModel.findOne({ "userId": data.userId }, (err, result) => {
+		callback(err, result);
+	})
 }
 
 const getUserInfo = function(userId, callback) {
-    loginModel.findOne({ "userId": userId }, (err, result) => {
-        callback(err, result);
-    });
+	loginModel.findOne({ "userId": userId }, (err, result) => {
+		callback(err, result);
+	});
 }
 
 const getChatList = function(userId, callback) {
-    loginModel.find({ 'online': 'Y', 'socketId': { $ne: userId } }, (err, result) => {
-        console.log(JSON.stringify(result, null, 2));
-        callback(err, result);
-    });
+	loginModel.find({ 'online': 'Y', 'socketId': { $ne: userId } }, (err, result) => {
+		console.log(JSON.stringify(result, null, 2));
+		callback(err, result);
+	});
 }
 
 const insertMessages = function(data, callback) {
-    let msgModel = new chatMsgModel(data);
-    msgModel.save((err, result) => {
-        callback(err, result);
-    });
+	let msgModel = new chatMsgModel(data);
+	msgModel.save((err, result) => {
+		callback(err, result);
+	});
 }
 
 const getMessages = function(userId, toUserId, callback) {
-    console.log("GET messages========================= " + userId)
+	const data = {
+		'$or': [{
+			'$and': [{
+				'toUserId': userId
+			}, {
+				'fromUserId': toUserId
+			}]
+		}, {
+			'$and': [{
+				'toUserId': toUserId
+			}, {
+				'fromUserId': userId
+			}]
+		}, ]
+	};
 
-    const data = {
-        '$or': [{
-            '$and': [{
-                'toUserId': userId
-            }, {
-                'fromUserId': toUserId
-            }]
-        }, {
-            '$and': [{
-                'toUserId': toUserId
-            }, {
-                'fromUserId': userId
-            }]
-        }, ]
-    };
-
-    chatMsgModel
-        .find(data)
-        .sort({ 'timestamp': 1 })
-        .exec((err, result) => {
-            callback(err, result)
-        })
+	chatMsgModel
+		.find(data)
+		.sort({ 'timestamp': 1 })
+		.exec((err, result) => {
+			callback(err, result)
+		})
 
 }
 
 const addSocketId = function(data, callback) {
-
-    loginModel.update({ "userId": data.id }, data.value, { upsert: true, 'new': true }, (err, result) => {
-        result.userId = data.id;
-        callback(err, result);
-    })
-
+	loginModel.update({ "userId": data.id }, data.value, { upsert: true, 'new': true }, (err, result) => {
+		result.userId = data.id;
+		callback(err, result);
+	})
 }
 
 module.exports = {
-    userNameCheck: userNameCheck,
-    login: login,
-    registerUser: registerUser,
-    userSessionCheck: userSessionCheck,
-    getUserInfo: getUserInfo,
-    getChatList: getChatList,
-    insertMessages: insertMessages,
-    getMessages: getMessages,
-    logout: logout,
-    addSocketId: addSocketId
+	userNameCheck: userNameCheck,
+	login: login,
+	registerUser: registerUser,
+	userSessionCheck: userSessionCheck,
+	getUserInfo: getUserInfo,
+	getChatList: getChatList,
+	insertMessages: insertMessages,
+	getMessages: getMessages,
+	logout: logout,
+	addSocketId: addSocketId
 };
--- server/api/chat/chat.router.js
+++ server/api/chat/chat.router.js
@@ -6,86 +6,76 @@ const chatEntity = require('./chat.entity')
 
 router.post('/usernameCheck', (request, response) => {
 
-    if (request.body.userName === "") {
-        response.status(412).json({
-            error: true,
-            message: `username cant be empty.`
-        });
-    } else {
-        helper.userNameCheck({
-            userName: request.body.userName.toLowerCase()
-        }, (count) => {
-
-            let result = {};
-
-            if (count > 0) {
-                result.error = true;
-            } else {
-                result.error = false;
-            }
-            response.status(200).json(result);
-        });
-    }
+	if (request.body.userName === "") {
+		response.status(412).json({
+			error: true,
+			message: `username cant be empty.`
+		});
+	} else {
+		helper.userNameCheck({
+			userName: request.body.userName.toLowerCase()
+		}, (count) => {
+			let result = {};
+			if (count > 0) {
+				result.error = true;
+			} else {
+				result.error = false;
+			}
+			response.status(200).json(result);
+		});
+	}
 });
 
 router.post('/userSessionCheck', (request, response) => {
 
-    let userId = request.body.userId;
-    let sessionCheckResponse = {}
+	let userId = request.body.userId;
+	let sessionCheckResponse = {}
 
-    if (userId == '') {
-        sessionCheckResponse.error = true;
-        sessionCheckResponse.message = `User Id cant be empty.`;
-        response.status(412).json(sessionCheckResponse);
-    } else {
+	if (userId == '') {
+		sessionCheckResponse.error = true;
+		sessionCheckResponse.message = `User Id cant be empty.`;
+		response.status(412).json(sessionCheckResponse);
+	} else {
 
-        helper.userSessionCheck({
-            userId: userId,
-        }, (error, result) => {
-
-            if (error || result === null) {
-
-                sessionCheckResponse.error = true;
-                sessionCheckResponse.message = `Server error.`;
-                response.status(503).json(sessionCheckResponse);
-            } else {
-
-                sessionCheckResponse.error = false;
-                sessionCheckResponse.userName = result.userName;
-                sessionCheckResponse.message = `User logged in.`;
-                response.status(200).json(sessionCheckResponse);
-            }
-        });
-    }
+		helper.userSessionCheck({
+			userId: userId,
+		}, (error, result) => {
+			if (error || result === null) {
+				sessionCheckResponse.error = true;
+				sessionCheckResponse.message = `Server error.`;
+				response.status(503).json(sessionCheckResponse);
+			} else {
+				sessionCheckResponse.error = false;
+				sessionCheckResponse.userName = result.userName;
+				sessionCheckResponse.message = `User logged in.`;
+				response.status(200).json(sessionCheckResponse);
+			}
+		});
+	}
 });
 
 router.post('/getMessages', (request, response) => {
-    let userId = request.body.userId;
-    let toUserId = request.body.toUserId;
-    let messages = {}
+	let userId = request.body.userId;
+	let toUserId = request.body.toUserId;
+	let messages = {}
 
-    if (userId == '') {
-        messages.error = true;
-        messages.message = `userId cant be empty.`;
-        response.status(200).json(messages);
-    } else {
-
-        helper.getMessages(userId, toUserId, (error, result) => {
-
-            if (error) {
-
-                messages.error = true;
-                messages.message = `Server error.`;
-                response.status(200).json(messages);
-
-            } else {
-
-                messages.error = false;
-                messages.messages = result;
-                response.status(200).json(messages);
-            }
-        });
-    }
+	if (userId == '') {
+		messages.error = true;
+		messages.message = `userId cant be empty.`;
+		response.status(200).json(messages);
+	} else {
+		helper.getMessages(userId, toUserId, (error, result) => {
+			if (error) {
+				messages.error = true;
+				messages.message = `Server error.`;
+				response.status(200).json(messages);
+			} else {
+				messages.error = false;
+				messages.messages = result;
+				response.status(200).json(messages);
+			}
+		});
+	}
 });
 
 module.exports = router;
