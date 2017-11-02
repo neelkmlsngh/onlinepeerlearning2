47770deddf91d45dce9bfe6d53945f9e106b35c2
shubhang.5.gupta <shubhang.5.gupta@niit-tech.com>
2017-11-02 12:17:06 +0530

    Added

--- server/config/app.js
+++ server/config/app.js
@@ -6,21 +6,10 @@ module.exports = {
   SALT_WORK_FACTOR : 10,
   PORT:8080,
   REDIRECT:'https://192.168.252.33:8080/#/auth/'
-  /*REDIRECT:'https://localhost:8080/#/auth/'*/
-=======
-    APPNAME: 'Onlinepeerlearning',
-    SECRET: 'secretkey',
-    EXPIRETIME: 86400,
-    SALT_WORK_FACTOR: 10,
-    PORT: 8080,
-    /*REDIRECT: 'https://192.168.252.152:8080/#/auth/',*/
-    REDIRECT: 'https://localhost:8080/#/auth/',
-    /*CLIENT_ID: 'd800be26e1dd031f7308',
-    CLIENT_SECRET: '860ee65d392ba8636752959a3e6baec090baf94e@',
-    CALLBACK_URL: "https://192.168.252.152:8080/auth/github/callback"*/
-    ,
+  
+
     CLIENT_ID: '7328322e0495591f5a69',
     CLIENT_SECRET: 'aac0e311b9be3dbd2fbe98cd23e3fa5fc60ea32c@',
     CALLBACK_URL: "https://localhost:8080/auth/github/callback"
->>>>>>> 09d2bf3d25d5168b1915248f5f74cc7973f5bf4b
+
 }
