1d015f0796ed6bc3d85cff1f49a18d26507874af
Sunil joshi <Sjoshi327@gmal.com>
2017-11-03 15:46:51 +0530

    testcases of services

--- client/src/app/shared/services/authentication.service.spec.ts
+++ client/src/app/shared/services/authentication.service.spec.ts
@@ -1,15 +1,49 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
+import {
+ MockBackend,
+ MockConnection
+} from '@angular/http/testing';
 
-import { AuthenticationService } from './authentication.service';
+import { HttpModule, Http, XHRBackend, Response, ResponseOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/toPromise';
+import { AuthenticationService as authenticationService } from './authentication.service';
 
-describe('AuthenticationService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [AuthenticationService]
-    });
-  });
+describe('profileService ,(mockBackend)', () => {
 
-  it('should be created', inject([AuthenticationService], (service: AuthenticationService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+
+ /*Initial configuration that will run before every testcase*/
+ beforeEach(() => {
+   TestBed.configureTestingModule({
+     imports: [HttpModule],
+     providers: [
+       authenticationService,
+       { provide: XHRBackend, useClass: MockBackend }
+     ]
+   });
+ });
+
+ /*Testcase to check whether service is injected or not*/
+ it('can instantiate service when inject service',
+   inject([authenticationService], (service: authenticationService) => {
+     expect(service instanceof authenticationService).toBe(true);
+   }));
+
+ /*Testcase to check whether mockdata is used instead of real database */
+ it('can provide the mockBackend as XHRBackend',
+   inject([XHRBackend], (backend: MockBackend) => {
+     expect(backend).not.toBeNull('backend should be provided');
+   }));
+
+ /*Testcase to check whether instance of service is created or not*/
+/* it('can instantiate service with "new"', inject([Http,Router], (http: Http,router: Router) => {
+   expect(http).not.toBeNull('http should be provided');
+   let service = new authenticationService(http,router);
+   expect(service instanceof authenticationService).toBe(true, 'new service should be ok');
+ }));*/
+
+})
