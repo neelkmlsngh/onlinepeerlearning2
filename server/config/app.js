889ae445a3edfdfc39e6ce0658457a414c744ab2
yash15196 <guptayash15196@gmail.com>
2017-11-04 12:15:53 +0530

    login

--- server/config/app.js
+++ server/config/app.js
@@ -10,8 +10,7 @@ module.exports = {
     FAILURE_REDIRECT: 'https://localhost:8080/#',
     CLIENT_ID: 'd800be26e1dd031f7308',
     CLIENT_SECRET: '666fc86271db872db67ed4a8ca50f4b5a2b95c8b',
-    CALLBACK_URL: "https://192.168.252.152:8080/api/login/auth/github/callback",
-    URL: "https://192.168.252.152:8080/"
+    CALLBACK_URL: "https://localhost:8080/api/login/auth/github/callback",
+    URL: "https://192.168.252.57:8080/"
 }
 
-}
