2ac5e944bae476d9941d282bfd2b1939ffe4ab3a
tanupreet_singh <tanupreet_singh@yahoo.com>
2017-11-07 10:05:59 +0530

    javascript editor fixed and password hidden for token generate

--- client/src/app/shared/config/config.ts
+++ client/src/app/shared/config/config.ts
@@ -11,11 +11,11 @@ export const config: any = {
   },
   connect:
   {
-    apiURL : "https://192.168.253.1:8080",
-    vmURL : "https://192.168.253.1:3030",
-    CALLBACK_URL:"https://192.168.253.1:8080/api/login/auth/github",
-    CLIENT_ID : 'f9ea78d1f4ead499cd22',
-    CLIENT_SECRET : '5a9f55cb5eaa65140a5949fb6595e0283c667c72'
+    apiURL : "https://192.168.252.204:8080",
+    vmURL : "https://192.168.252.204:3030",
+    CALLBACK_URL:"https://192.168.252.204:8080/api/login/auth/github",
+    CLIENT_ID : 'eda558d85f4b8157078f',
+    CLIENT_SECRET : '2456afab849c7fd43d4977de7f1187b8d5e74329'
   },
 
   giturls: {
@@ -35,7 +35,7 @@ export const config: any = {
     "SEARCHAPIURL": 'api/forums/term/'
   },
   peerserver: {
-    "host": "192.168.253.1",
+    "host": "192.168.252.204",
     "port": "8081",
     "path": "/peerjs"
 
