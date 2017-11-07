6839932c034c5207389e989c62072a3a66b482d5
PrakharPandey <1prakharpandey1@gmail.com>
2017-11-07 13:37:56 +0530

    sd

--- client/src/app/shared/config/config.ts
+++ client/src/app/shared/config/config.ts
@@ -11,11 +11,11 @@ export const config: any = {
   },
   connect:
   {
-    apiURL : "https://192.168.253.1:8080",
-    vmURL : "https://192.168.253.1:3030",
-    CALLBACK_URL:"https://192.168.253.1:8080/api/login/auth/github",
-    CLIENT_ID : 'f9ea78d1f4ead499cd22',
-    CLIENT_SECRET : '5a9f55cb5eaa65140a5949fb6595e0283c667c72'
+    apiURL : "https://192.168.252.207:8080",
+    vmURL : "https://192.168.252.207:3030",
+    CALLBACK_URL:"https://192.168.252.207:8080/api/login/auth/github",
+    CLIENT_ID : 'ff1442095291468e2b88',
+    CLIENT_SECRET : '008206b51cc54644f7cb921bf8623fe595c1b597'
   },
 
   giturls: {
--- server/config/app.js
+++ server/config/app.js
@@ -4,12 +4,12 @@ module.exports = {
   EXPIRETIME: 86400,
   SALT_WORK_FACTOR: 10,
   PORT: 8080,
-  SUCCESS_REDIRECT: 'https://192.168.253.1:8080/#/auth/',
-  FAILURE_REDIRECT: 'https://192.168.253.1:8080/#',
-  CLIENT_ID: 'f9ea78d1f4ead499cd22',
-  CLIENT_SECRET: '5a9f55cb5eaa65140a5949fb6595e0283c667c72',
-  CALLBACK_URL: "https://192.168.253.1:8080/api/login/auth/github/callback",
-  URL: "https://192.168.253.1:8080/"
+  SUCCESS_REDIRECT: 'https://192.168.252.207:8080/#/auth/',
+  FAILURE_REDIRECT: 'https://192.168.252.207:8080/#',
+  CLIENT_ID: 'ff1442095291468e2b88',
+  CLIENT_SECRET: '008206b51cc54644f7cb921bf8623fe595c1b597',
+  CALLBACK_URL: "https://192.168.252.207:8080/api/login/auth/github/callback",
+  URL: "https://192.168.252.207:8080/"
 }
 
 
