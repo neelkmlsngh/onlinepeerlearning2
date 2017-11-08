a95b6bd66026a42a84fab3a4840c94059e9d2da0
tanupreet_singh <tanupreet_singh@yahoo.com>
2017-11-08 15:36:20 +0530

    config file fixed

--- server/config/app.js
+++ server/config/app.js
@@ -5,10 +5,10 @@ module.exports = {
   EXPIRETIME: 86400,
   SALT_WORK_FACTOR: 10,
   PORT: 8080,
-  SUCCESS_REDIRECT: 'https://192.168.252.209:8080/#/auth/',
-  FAILURE_REDIRECT: 'https://192.168.252.209:8080/#',
-  CLIENT_ID: '0c5e4d981e5db1ee5bb4',
-  CLIENT_SECRET: '50d37ae458e50eb456de66aea66573c80869ea0d',
-  CALLBACK_URL: "https://192.168.252.209:8080/api/login/auth/github/callback",
-  URL: "https://192.168.252.209:8080/"
+  SUCCESS_REDIRECT: 'https://192.168.252.204:8080/#/auth/',
+  FAILURE_REDIRECT: 'https://192.168.252.204:8080/#',
+  CLIENT_ID : 'eda558d85f4b8157078f',
+  CLIENT_SECRET : '2456afab849c7fd43d4977de7f1187b8d5e74329',
+  CALLBACK_URL: "https://192.168.252.204:8080/api/login/auth/github/callback",
+  URL: "https://192.168.252.204:8080/"
 }
