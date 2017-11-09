81808ca71b3d9adce29964b4ace1224fdab2fe59
tanupreet_singh <tanupreet_singh@yahoo.com>
2017-11-09 12:01:38 +0530

    forum test cases added @prakhar

--- server/test/forum.test.js
+++ server/test/forum.test.js
@@ -1,110 +1,203 @@
-let app = require('./../app/app');
-const forumModel = require('./../api/forums/forum.entity');
-
-let expect = require('chai').expect
+//Providing Testing Environment
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+//Importing third party libraries to test api
 let supertest = require('supertest')
-let sinon = require('sinon');
-let forumGetStub = sinon.stub(forumModel, 'find');
-
-/*let model = require("../model/empSchema")
-let modelStub = sinon.stub(model, 'find')
-let modelsave = sinon.stub(model.prototype, 'save')
-let modelupdate = sinon.stub(model, 'update')
-let modeldelete = sinon.stub(model, 'remove')*/
-var url = supertest("https://localhost:8080")
-
-
-describe('test fetch data of locationchange access card', () => {
-
- /*   before(() => {
-        forumGetStub.yields(null, {
-            questionTitle: 'What is a NullPointerException, and how do I fix it?',
-            problemDescription: 'What are Null Pointer Exceptions (java.lang.NullPointerException) and what causes them? What methods/tools can be used to determine the cause so that you stop the exception from causing the program ',
-            codeSnippet: '<html></html>',
-            tags: 'Angularjs,Nodejs',
-            date: '21/11/2017'
-        });
-    });
-*/
-    forumGetStub.yields(null, {"name": "Prakhar"})
-    it("find employees", function(done) {
-       
-        url
-            .get("/api/forums")
-     /*       .expect(201)
-            .expect('Content-Type', /json/)*/
-            .end(function(err, res) {
-              /*  if (err) {
-                    throw err
-                }*/
-                console.log("err"+err);
-                console.log("res"+res);
-                expect(res.body.name).to.be.equal('Prakhar')
-                done();
-
-            })
+let expect = require('chai').expect;
+let sinon = require('sinon')
+//Importing required modules
+var app = require('../bin/www');
+let model = require("../api/forums/forum.entity");
+//Importing stubs for different types of requests
+let modelStub = sinon.stub(model, 'find');
+let modelStubSave = sinon.stub(model.prototype, 'save');
+let modelStubFindOne = sinon.stub(model, 'findOne');
+let modelStubUpdate = sinon.stub(model, 'update');
+//Test Suite for Forum//
+describe("Testing Employee", () => {
+    //Providing Stubs for different requests
+    beforeEach(() => {
+        modelStubSave.yields(null, { username: "Prakhar", questionTitle: "What?", problemDescription: "Not Working", date: "9/11/2017",
+        time: "10:34AM", votes: 45, codeSnippet: "import * from '@angular/core'", tags: "Angular" });
+        modelStub.yields(null, { username: "Prakhar", questionTitle: "What?", problemDescription: "Not Working", date: "9/11/2017",
+        time: "10:34AM", votes: 45, codeSnippet: "import * from '@angular/core'", tags: "Angular" });
+        modelStubFindOne.yields(null, { username: "Prakhar", questionTitle: "What?", problemDescription: "Not Working", date: "9/11/2017",
+        time: "10:34AM", votes: 45, codeSnippet: "import * from '@angular/core'", tags: "Angular" });
+        modelStubUpdate.yields(null, { username: "Prakhar", questionTitle: "What?", problemDescription: "Not Working", date: "9/11/2017",
+        time: "10:34AM", votes: 45, codeSnippet: "import * from '@angular/core'", tags: "Angular" });
     })
-
-    //positive test case for fetch location change record    
-/*    it('positive case for getting data from forum', (done) => {
-        supertest(url)
-            .get('/api/forums')
-            .expect(201)
-            .expect('Content-Type', /json/)
-            .end((err, res) => {
-                if (err) return done(err);
-                console.log("res"+res)
-                expect(res.body.data. questionTitle).to.equal('What is a NullPointerException, and how do I fix it?');
-                done();
-            });
-    });*/
-
-    //negative test case for fetch location change record  
-/*    it(' negative case of from forum', (done) => {
-        supertest(url)
-            .get('/locationchange/findlocation')
+    //---Positive Test Cases---//
+    //Test Case for adding new posts in the forum
+    it('adding forum posts', (done) => {
+        supertest(app)
+            .post('/api/forums')
+            .send({ username: "Sunil" })
             .expect(200)
             .expect('Content-Type', /json/)
             .end((err, res) => {
-                if (err) return done(err);
-                chai.expect(res.body.data. questionTitle).not.to.equal('What is a NullPointerException');
+                if (err) console.log(err);
+                expect(res.body.data.username).to.equal("Sunil");
                 done();
             });
-    });*/
-
-	before(() => {
-		forumGetStub.yields(null, {
-			questionTitle: 'What is a NullPointerException, and how do I fix it?',
-			problemDescription: 'What are Null Pointer Exceptions (java.lang.NullPointerException) and what causes them? What methods/tools can be used to determine the cause so that you stop the exception from causing the program ',
-			codeSnippet: '<html></html>',
-			tags: 'Angularjs,Nodejs',
-			date: '21/11/2017'
-		});
-	});
-	//positive test case for fetch location change record    
-	/*it('positive case for getting data from forum', (done) => {
-		supertest(url)
-			.get('/')
-			.expect(200)
-			.expect('Content-Type', /json/)
-			.end((err, res) => {
-				if (err) return done(err);
-				chai.expect(res.body.data. questionTitle).to.equal('What is a NullPointerException, and how do I fix it?');
-				done();
-			});
-	});*/
-
-	//negative test case for fetch location change record  
-/*	it(' negative case of from forum', (done) => {
-		supertest(url)
-			.get('/locationchange/findlocation')
-			.expect(200)
-			.expect('Content-Type', /json/)
-			.end((err, res) => {
-				if (err) return done(err);
-				chai.expect(res.body.data. questionTitle).not.to.equal('What is a NullPointerException');
-				done();
-			});
-	});*/
-
+    });
+    //Test Case for getting all posts in the forum
+    it('getting forum posts', (done) => {
+        supertest(app)
+            .get('/api/forums')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.equal("Prakhar");
+                done();
+            });
+    });
+    //Test Case for searching posts in the forum
+    it('getting search results', (done) => {
+        supertest(app)
+            .get('/api/forums/term/abc')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.equal("Prakhar");
+                done();
+            });
+    });
+    //Test Case for getting posts by id in the forum
+    it('getting forum post by id', (done) => {
+        supertest(app)
+            .get('/api/forums/56')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.equal("Prakhar");
+                done();
+            });
+    });
+    //Test Case for saving answers in the forum
+    it('saving answer to a forum post', (done) => {
+        supertest(app)
+            .put('/api/forums/23')
+            .send({username: "Sunil"})
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.equal("Prakhar");
+                done();
+            });
+    });
+    //Test Case for adding like to a post in the forum
+    it('adding like to a forum post', (done) => {
+        supertest(app)
+            .put('/api/forums/term/56')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.equal("Prakhar");
+                done();
+            });
+    });
+    //Test Case for adding dislike to a post in the forum
+    it('adding dislike to a forum post', (done) => {
+        supertest(app)
+            .put('/api/forums/dislike/56')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.equal("Prakhar");
+                done();
+            });
+    });
+    //---Negative Test Cases---//
+    
+    //Test Case for adding new posts in the forum
+    it('adding forum posts', (done) => {
+        supertest(app)
+            .post('/api/forums')
+            .send({ username: "Sunil" })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.not.equal("Prakhar");
+                done();
+            });
+    });
+    //Test Case for getting all posts in the forum
+    it('getting forum posts', (done) => {
+        supertest(app)
+            .get('/api/forums')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.not.equal("Sunil");
+                done();
+            });
+    });
+    //Test Case for searching posts in the forum
+    it('getting search results', (done) => {
+        supertest(app)
+            .get('/api/forums/term/abc')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.not.equal("Sunil");
+                done();
+            });
+    });
+    //Test Case for getting posts by id in the forum
+    it('getting forum post by id', (done) => {
+        supertest(app)
+            .get('/api/forums/56')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.not.equal("Sunil");
+                done();
+            });
+    });
+    //Test Case for saving answers in the forum
+    it('saving answer to a forum post', (done) => {
+        supertest(app)
+            .put('/api/forums/23')
+            .send({username: "Sunil"})
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.not.equal("Sunil");
+                done();
+            });
+    });
+    //Test Case for adding like to a post in the forum
+    it('adding like to a forum post', (done) => {
+        supertest(app)
+            .put('/api/forums/term/56')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.not.equal("Sunil");
+                done();
+            });
+    });
+    //Test Case for adding dislike to a post in the forum
+    it('adding dislike to a forum post', (done) => {
+        supertest(app)
+            .put('/api/forums/dislike/56')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res.body.data.username).to.not.equal("Sunil");
+                done();
+            });
+    });
 });
