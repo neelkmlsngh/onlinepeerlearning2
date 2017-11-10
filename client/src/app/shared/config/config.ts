144324fe8636ae4db15ac21cd2a232dfab136b59
GauravGupta131220 <grvgupta12@gmail.com>
2017-11-10 11:51:40 +0530

    changes

--- client/src/app/shared/config/config.ts
+++ client/src/app/shared/config/config.ts
@@ -1,6 +1,6 @@
 export const config: any = {
 
- language: [
+  language: [
     "Javascript",
     "Html",
     "CSS"
@@ -16,12 +16,14 @@ export const config: any = {
       "REMOVED_PLUGINS": "list,basicstyles,wsc,scayt,about,specialchar,scayt,spellchecker,elementspath,resize"
     }
   },
+
   connect: {
-    apiURL : "https://192.168.252.152:8080",
-    vmURL : "https://192.168.252.152:3030",
-    CALLBACK_URL:"https://192.168.252.152:8080/api/login/auth/github",
-    CLIENT_ID : 'd800be26e1dd031f7308',
-    CLIENT_SECRET : '4f34a102e4b2d71b77246520afee60d692da9b5a'
+    apiURL: "https://192.168.252.151:8080",
+    vmURL: "https://192.168.252.151:3030",
+    CALLBACK_URL: "https://192.168.252.151:8080/api/login/auth/github",
+    CLIENT_ID: '71e507179821e4c52cc6',
+    CLIENT_SECRET: 'f5a19047f54acc10db755689779bb23681ec5496'
+
   },
 
   giturls: {
@@ -41,7 +43,7 @@ export const config: any = {
     "SEARCHAPIURL": 'api/forums/term/'
   },
   peerserver: {
-    "host": "192.168.252.152",
+    "host": "192.168.252.151",
     "port": "8081",
     "path": "/peerjs"
   }
