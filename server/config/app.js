839c1b7d6f1b9a39f8ea0afd9bc23f743a25829d
GauravGupta131220 <grvgupta12@gmail.com>
2017-11-11 10:28:59 +0530

    new changes

--- server/config/app.js
+++ server/config/app.js
@@ -1,18 +1,14 @@
 module.exports = {
 
-
     APPNAME: 'Onlinepeerlearning',
     SECRET: 'secretkey',
     EXPIRETIME: 86400,
     SALT_WORK_FACTOR: 10,
     PORT: 8080,
-
-
     SUCCESS_REDIRECT: 'https://192.168.252.151:8080/#/auth/',
     FAILURE_REDIRECT: 'https://192.168.252.151:8080/#',
     CLIENT_ID: '71e507179821e4c52cc6',
     CLIENT_SECRET: 'f5a19047f54acc10db755689779bb23681ec5496',
     CALLBACK_URL: "https://192.168.252.151:8080/api/login/auth/github/callback",
     URL: "https://192.168.252.152:8080/"
-
 }
