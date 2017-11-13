ad5a480d33adb6c789e9951778b6a9a0f2b48ce8
pulkit.5.kakkar <pulkit.5.kakkar@niit-tech.com>
2017-11-13 17:19:01 +0530

    update by tanupreet

--- client/src/app/shared/config/config.ts
+++ client/src/app/shared/config/config.ts
@@ -1,11 +1,11 @@
 export const config: any = {
   
   connect: {
-    apiURL: "https://192.168.253.4:8080",
+    apiURL: "https://192.168.252.152:8080",
     vmURL: "https://192.168.252.152:3030",
-    CALLBACK_URL: "https://192.168.253.4:8080/api/login/auth/github",
-    CLIENT_ID: '96cb5f2d1d9fdce76884',
-    CLIENT_SECRET: '41aab272f81b7a881635739e1c0d27335b753d00'
+    CALLBACK_URL: "https://192.168.252.152:8080/api/login/auth/github",
+    CLIENT_ID: 'd800be26e1dd031f7308',
+    CLIENT_SECRET: '4f34a102e4b2d71b77246520afee60d692da9b5a'
   },
 
   giturls: {
